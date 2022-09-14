e64f413bb5494b25dbe48b76c7ddfe94f63afa60
Mariany Lovera <mariany.lovera.sanchez@ciisa.cl>
2022-09-14 01:15:03 -0300

    Se utilizo props y se modifico imagenes

--- testimonios/src/App.js
+++ testimonios/src/App.js
@@ -6,7 +6,30 @@ function App() {
     <div className="App">
       <div className="contenedor-principal">
         <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp: </h1>
-        <Testimonio />
+        <Testimonio
+          nombre="Emma Bostian"
+          pais="Suecia"
+          imagen="emma"
+          cargo="Ingeniera de Software"
+          empresa="Spotify"
+          testimonio="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus animi, culpa inventore voluptatibus aliquam incidunt consectetur illum quis. Similique excepturi ipsum sunt commodi dicta maxime quia voluptates facilis officia sequi!"
+        />
+        <Testimonio
+          nombre="Shawn Wang"
+          pais="Singapur"
+          imagen="shawn"
+          cargo="Ingeniero de Software"
+          empresa="Amazon"
+          testimonio="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus animi, culpa inventore voluptatibus aliquam incidunt consectetur illum quis. Similique excepturi ipsum sunt commodi dicta maxime quia voluptates facilis officia sequi!"
+        />
+        <Testimonio
+          nombre="Camila Chima"
+          pais="Nigeria"
+          imagen="camila"
+          cargo="Ingeniera de Software"
+          empresa="ChatDesk"
+          testimonio="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus animi, culpa inventore voluptatibus aliquam incidunt consectetur illum quis. Similique excepturi ipsum sunt commodi dicta maxime quia voluptates facilis officia sequi!"
+        />
       </div>
     </div>
   );
--- testimonios/src/componentes/Testimonio.js
+++ testimonios/src/componentes/Testimonio.js
@@ -1,23 +1,23 @@
 import React from "react";
+import "../hojas-de-estilos/Testimonio.css";
 
-function Testimonio() {
+function Testimonio(props) {
   //componente
   return (
     <div className="contenedor-testimonio">
       <img
         className="imagen-testimonio"
-        src={require("../imagenes/face1.jpg")}
+        src={require(`../imagenes/testimonio-${props.imagen}.jpg`)}
         alt="Foto de Emma"
       />
       <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio">Emma Bostian en Suecia</p>
-        <p className="cargo-testimonio">Ingeniera de Software en Spotify</p>
-        <p className="texto-testimonio">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum
-          inventore sit exercitationem laudantium perspiciatis assumenda at
-          natus, veniam impedit asperiores officia recusandae nulla quae, illum
-          sequi ut iure, labore tempora.{" "}
+        <p className="nombre-testimonio">
+          <strong>{props.nombre}</strong> en {props.pais}
         </p>
+        <p className="cargo-testimonio">
+          {props.cargo} en <strong>{props.empresa}</strong>
+        </p>
+        <p className="texto-testimonio">"{props.testimonio}"</p>
       </div>
     </div>
   );
